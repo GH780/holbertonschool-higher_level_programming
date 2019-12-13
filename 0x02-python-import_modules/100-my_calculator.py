f1e52d1d14618bfbd7307f828a931622d6684c41
mmanumos <mosqueramanuel5@gmail.com>
2019-12-13 03:50:47 +0000

    [Feature] Execute an operation depending the operator new file:   100-my_calculator.py

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    from calculator_1 import *
+    a = 0
+    val = False
+    op = {"+":add, "-":sub, "*": mul, "/":div}
+
+    if len(argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    else:
+        for i in op:
+            if argv[2] == i:
+                a = op[i](int(argv[1]),int(argv[3]))
+                val = True
+                break;
+
+    if val == False:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+
+    print(str(argv[1]), "{}".format(i), str(argv[3]), "= {:d}".format(a))
+    exit(0)
