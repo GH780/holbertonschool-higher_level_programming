330cb48465dde4a291587ed86d575d0bd547660d
mmanumos <mosqueramanuel5@gmail.com>
2019-12-13 03:56:11 +0000

    [Fix] Styles for pep8 modified:   100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     from calculator_1 import *
     a = 0
     val = False
-    op = {"+":add, "-":sub, "*": mul, "/":div}
+    op = {"+": add, "-": sub, "*": mul, "/": div}
 
     if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
@@ -12,11 +12,11 @@ if __name__ == "__main__":
     else:
         for i in op:
             if argv[2] == i:
-                a = op[i](int(argv[1]),int(argv[3]))
+                a = op[i](int(argv[1]), int(argv[3]))
                 val = True
-                break;
+                break
 
-    if val == False:
+    if val is False:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
 
