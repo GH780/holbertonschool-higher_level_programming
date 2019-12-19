4d3e009fcb8f1a337d6d77bdb4b84b41ec71c9a1
mmanumos <mosqueramanuel5@gmail.com>
2019-12-19 20:49:01 +0000

    [Fix] The validation is removed modified:   0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    if len(matrix) == 1 and any(matrix) is False:
-        return
-
-    new_matrix = []
-
     for x in matrix:
         val = list(map(lambda mul: mul ** 2, x))
         new_matrix.append(val)
