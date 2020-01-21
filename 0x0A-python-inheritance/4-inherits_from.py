83e94bd373cda93bcc67e4bb59f742f88d127167
mmanumos <mosqueramanuel5@gmail.com>
2020-01-21 20:17:05 +0000

    [Fix] The function is left as it was before modified:   4-inherits_from.py

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-'''function that returns True if the object is an instance of a class
-   that inherited (directly or indirectly) from the specified class ;
-   otherwise False.
-'''
-
-
 def inherits_from(obj, a_class):
-    '''
-    Returns True or False
-    '''
-    return isinstance(obj, a_class) and not isinstance(obj, a_class)
+    """ Returns true if the object is exactly an instance of the a_class
+        or an instance of the superclass
+    """
+    if isinstance(obj, a_class) and issubclass(a_class, type(obj)):
+        return False
+    else:
+        return True
