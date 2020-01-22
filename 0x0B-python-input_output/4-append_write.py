768e303d5f04ac5e7170cb289431874909e34f07
mmanumos <mosqueramanuel5@gmail.com>
2020-01-22 02:42:11 +0000

    [Feature] Append content to a file new file:   4-append_write.py

---
+++ 0x0B-python-input_output/4-append_write.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def append_write(filename="", text=""):
+    """ Append content to a file, it doesn´t exist then is created """
+    with open(filename, "a+", encoding="utf-8") as writer:
+        return writer.write(text)
