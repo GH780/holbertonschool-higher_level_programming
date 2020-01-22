d21e210dc58dc83404ae14698f960087cb8e00cb
mmanumos <mosqueramanuel5@gmail.com>
2020-01-22 21:05:24 +0000

    [Feature] Write a new file with the result of convert fro Python to JSON modified:   7-save_to_json_file.py

--- 0x0B-python-input_output/7-save_to_json_file.py
+++ 0x0B-python-input_output/7-save_to_json_file.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-"""
-Python Function that writs an object to a file
-Using JSON representation
-"""
-
 import json
 
 
 def save_to_json_file(my_obj, filename):
-    with open(filename, 'w', encoding='utf-8') as f:
-        f.write(json.dumps(my_obj))
+    """ Returns an object python
+        it could also be said that it converts from JSON to python
+    """
+    with open(filename, "w") as writer:
+        writer.write(json.dumps(my_obj))
