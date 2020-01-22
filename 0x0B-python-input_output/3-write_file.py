8ea7fbec44c67e65a7965b6ac002a8e9a347beaf
mmanumos <mosqueramanuel5@gmail.com>
2020-01-22 02:29:25 +0000

    [Fix] The encoding utf-8 is added modified:   3-write_file.py

--- 0x0B-python-input_output/3-write_file.py
+++ 0x0B-python-input_output/3-write_file.py
@@ -3,5 +3,5 @@ def write_file(filename="", text=""):
     """ Write a file with given string and returns the numbers of characters
         writed
     """
-    with open(filename, "w") as writer:
+    with open(filename, "w", encoding="utf-8") as writer:
         return writer.write(text)
