c97f8e51dc1877c80f95484dcc34b742f8d61a25
mmanumos <mosqueramanuel5@gmail.com>
2020-01-23 01:49:11 +0000

    [Feature] Script that create a python list and saves it into a json file modified:   9-add_item.py

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-"""
-Python function that add arguments to a Python list
-"""
+import json
 import sys
 import os
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
-save_json = __import__('7-save_to_json_file').save_to_json_file
-load_json = __import__('8-load_from_json_file').load_from_json_file
 
-my_list = []
+list_1 = []
 
 if not os.path.exists("./add_item.json"):
-    save_json(my_list, "add_item.json")
-my_list = load_json("add_item.json")
+    save_to_json_file(list_1, "add_item.json")
 
-for items in sys.argv[1:]:
-    my_list.append(items)
-save_json(my_list, "add_item.json")
+my_list = load_from_json_file("add_item.json")
+
+for i in sys.argv[1:]:
+    list_1.append(i)
+
+save_to_json_file(list_1, "add_item.json")
