b5bf6b9b359386927d52db8328fae563f63937d3
mmanumos <mosqueramanuel5@gmail.com>
2020-01-28 04:23:41 +0000

    [Fix] The creation for dummy on the basis of Rectangle is changed. modified:   models/base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -50,7 +50,7 @@ class Base:
             a dictionary.
         """
         if cls.__name__ == "Rectangle":
-            dummy = cls(1, 1, 1, 1)
+            dummy = cls(1, 1)
         elif cls.__name__ == "Square":
             dummy = cls(1)
 
