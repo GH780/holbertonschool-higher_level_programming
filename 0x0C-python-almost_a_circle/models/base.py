2253fbc3900afd55a6f7874c5868622c4982fb61
mmanumos <mosqueramanuel5@gmail.com>
2020-01-28 04:18:34 +0000

    [Fix] The string is changed by an empty list modified:   models/base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -30,7 +30,7 @@ class Base:
     def from_json_string(json_string):
         """ Returns a Json representation of a Python object """
         if json_string is None or len(json_string) == 0:
-            return "[]"
+            return []
         else:
             return json.loads(json_string)
 
