877e9bb80c28c0a25c75cb7d1d1b32e33d1b94fa
mmanumos <mosqueramanuel5@gmail.com>
2020-03-03 21:55:51 +0000

    [Feature] UPPERCASE alias. modified:   13-count_shows_by_genre.sql

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- Select with right Join and double order by.
-SELECT tv_genres.name as genre,
-COUNT(*) as number_of_shows
+SELECT tv_genres.name AS genre,
+COUNT(*) AS number_of_shows
 FROM tv_genres
 JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
 JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
