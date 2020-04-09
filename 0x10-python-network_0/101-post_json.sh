62c3e598315e2ce91799cd17e2c971482944e8b5
Manuel Mosquera <mosqueramanuel5@gmail.com>
2020-04-09 18:39:13 +0000

    [Fix] Other order. modified:   101-post_json.sh

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A json is passed as argument in an send request with POST
-curl -sX POST "$1" -d @"$2" -H "Content-Type: application/json"
+curl "$1" -X POST -H "Content-Type: application/json" -d @"$2"
