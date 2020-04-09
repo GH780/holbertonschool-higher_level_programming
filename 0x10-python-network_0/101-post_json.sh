11cdabc862f9c0e6b9966fee94ae10e830e1b106
Manuel Mosquera <mosqueramanuel5@gmail.com>
2020-04-09 18:40:48 +0000

    [Fix] the option s is added. modified:   101-post_json.sh

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A json is passed as argument in an send request with POST
-curl "$1" -X POST -H "Content-Type: application/json" -d @"$2"
+curl "$1" -sX POST -H "Content-Type: application/json" -d @"$2"
