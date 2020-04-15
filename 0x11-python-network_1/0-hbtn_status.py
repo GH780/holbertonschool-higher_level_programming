a4fbafe5292f97913b9545d5aacc513c5c6c01ce
Manuel Mosquera <mosqueramanuel5@gmail.com>
2020-04-15 22:01:22 +0000

    [Fix] Pep8 styles. modified:   0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,5 +4,6 @@ import urllib.request as url
 with url.urlopen('https://intranet.hbtn.io/status') as response:
     response = response.read()
 
-print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(response), response))
+print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(response),
+                                                             response))
 print('\t- utf8 content: {}'.format(str(response, 'utf-8')))
