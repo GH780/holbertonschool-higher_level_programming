ab1be6336ccba80160dfa496e04c6d6797b02ee8
Manuel Mosquera <mosqueramanuel5@gmail.com>
2020-04-15 03:25:14 +0000

    [Feature] The state_code of a URL is showed using Request with parameter state_code. new file:   7-error_code.py

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import requests
+from sys import argv
+
+response = requests.get(argv[1])
+if response.status_code >= 400:
+    print("Error code: {}".format(response.status_code))
+else:
+    print(response.text)
