9d3305768a4d9c728e77efbd02c4e1cfd657723d
Manuel Mosquera <mosqueramanuel5@gmail.com>
2020-04-15 03:57:11 +0000

    [Fix] The Json library removed and the logical is changed. modified:   8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,20 +1,17 @@
 #!/usr/bin/python3
 import requests
 from sys import argv
-import json
 
 q = ""
 if len(argv) == 2:
     q = argv[1]
 
 response = requests.post('http://0.0.0.0:5000/search_user', data = {'q': q})
-
 try:
-    dict = json.loads(response.text)
+    dict = response.json()
     if any(dict):
         print("[{}] {}".format(dict['id'], dict['name']))
     else:
         print("No result")
-
-except ValueError:
+except:
     print("Not a valid JSON")
