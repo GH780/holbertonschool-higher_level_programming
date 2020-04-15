4b7d2de19abe04ed344e56a737a99946baea2067
Manuel Mosquera <mosqueramanuel5@gmail.com>
2020-04-15 21:57:42 +0000

    [Fix] The output is fixed again numbre 3. modified:   0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,7 +4,5 @@ import urllib.request as url
 with url.urlopen('https://intranet.hbtn.io/status') as response:
     response = response.read()
 
-print("Body response:")
-print("    - type: {}".format(type(response)))
-print("    - content: {}".format(response))
-print("    - utf-8 content: {}".format(str(response, 'utf-8')))
+print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(response), response))
+print('\t- utf8 content: {}'.format(str(response, 'utf-8')))
