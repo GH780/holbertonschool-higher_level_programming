9c751f7ab014612ed9c95043c277338d71b43a44
Manuel Mosquera <mosqueramanuel5@gmail.com>
2020-04-17 02:53:01 +0000

    [Fix] Fixed again 2 modified:   1-hbtn_header.py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,8 +2,9 @@
 # Python script that displays getheader value
 
 if __name__ == '__main__':
-    import urllib.request as ur
+    import urllib.request as url
     from sys import argv
 
-    with ur.urlopen(argv[1]) as header:
-        print(header.getheader('X-Request-Id'))
+    response = url.urlopen(argv[1])
+    header = response.info()
+    print(header['X-Request-Id'])
