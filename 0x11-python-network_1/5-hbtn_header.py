9e24a9641c064917bce11272c3deeae756f577f5
Manuel Mosquera <mosqueramanuel5@gmail.com>
2020-04-17 03:04:14 +0000

    [Fix] Styles and validation. modified:   5-hbtn_header.py

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-import requests 
-from sys import argv
+# Python script that fetches a url and get header value using only requests
+if __name__ == "__main__":
+    import requests 
+    from sys import argv
 
-response = requests.get(argv[1])
-h = response.headers.get('X-Request-Id')
-print(h)
+    response = requests.get(argv[1])
+    h = response.headers.get('X-Request-Id')
+    print(h)
