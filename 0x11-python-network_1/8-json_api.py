a72210c27ca2ee6cac473792e35994683a2f233f
Manuel Mosquera <mosqueramanuel5@gmail.com>
2020-04-17 03:16:22 +0000

    [Fix] Styles and validation. modified:   8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
-import requests
-from sys import argv
+# Python script that fetches a url and post email using only requests
+if __name__ == "__main__":
+    import requests
+    from sys import argv
 
-q = ""
-if len(argv) == 2:
-    q = argv[1]
+    q = ""
+    if len(argv) == 2:
+        q = argv[1]
 
-response = requests.post('http://0.0.0.0:5000/search_user', data = {'q': q})
-try:
-    dict = response.json()
-    if any(dict):
-        print("[{}] {}".format(dict['id'], dict['name']))
-    else:
-        print("No result")
-except:
-    print("Not a valid JSON")
+    response = requests.post('http://0.0.0.0:5000/search_user', data={'q': q})
+    try:
+        dict = response.json()
+        if any(dict):
+            print("[{}] {}".format(dict['id'], dict['name']))
+        else:
+            print("No result")
+    except:
+        print("Not a valid JSON")
