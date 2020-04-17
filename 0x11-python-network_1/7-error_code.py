f5bf33418400fd159e25e79c563e9f8cdf774abe
Manuel Mosquera <mosqueramanuel5@gmail.com>
2020-04-17 03:12:35 +0000

    [Fix] Styles and validation. modified:   7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-import requests
-from sys import argv
+# Python script that fetches a url and returns status code
+if __name__ == "__main__":
+    import requests
+    from sys import argv
 
-response = requests.get(argv[1])
-if response.status_code >= 400:
-    print("Error code: {}".format(response.status_code))
-else:
-    print(response.text)
+    response = requests.get(argv[1])
+    if response.status_code >= 400:
+        print("Error code: {}".format(response.status_code))
+    else:
+        print(response.text)
