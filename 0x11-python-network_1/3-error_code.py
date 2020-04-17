b8e785203522eeb1bba347fe95d3d123be5a6bf0
Manuel Mosquera <mosqueramanuel5@gmail.com>
2020-04-17 02:57:13 +0000

    [Feature] Validation for main is added. modified:   3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-import urllib.request as urr
-import urllib.error as ure
-from sys import argv
+# Python script to send request to URL and display error code
+if __name__ == "__main__":
+    import urllib.request as urr
+    import urllib.error as ure
+    from sys import argv
 
-try:
-    with urr.urlopen(argv[1]) as response:
-        response = response.read()
-        print("{}".format(str(response, 'utf-8')))
-except ure.URLError as e:
-    print("Error code: {}".format(e.code))
+    try:
+        with urr.urlopen(argv[1]) as response:
+            response = response.read()
+            print("{}".format(str(response, 'utf-8')))
+        except ure.URLError as e:
+            print("Error code: {}".format(e.code))
