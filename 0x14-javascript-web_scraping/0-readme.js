26d3eab2e104ba03d492a2a9b723a7f0f7e6c1bb
Manuel Mosquera <mosqueramanuel5@gmail.com>
2020-05-12 21:34:54 +0000

    [Fix] Error is showed. modified:   0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
 const modFs = require('fs');
 modFs.readFile(process.argv[2], 'utf-8', function (err, data) {
-  if (err) throw err;
-  console.log(data);
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
 });
