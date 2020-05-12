53f9f1fc6d46bdb09e4c4d8a2daa1d1839cd9151
Manuel Mosquera <mosqueramanuel5@gmail.com>
2020-05-12 23:14:39 +0000

    [Fix] Changing validation from a variable to includes. modified:   4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 const request = require('request');
-const charWed = 'https://swapi-api.hbtn.io/api/people/18/';
 let count = 0;
 request(process.argv[2], (error, response, body) => {
   if (error) { console.log(error); } else {
@@ -8,7 +7,7 @@ request(process.argv[2], (error, response, body) => {
     for (let x = 0; x < listFilm.length; x++) {
       const listCharacters = listFilm[x].characters;
       for (let y = 0; y < listCharacters.length; y++) {
-        if (listCharacters[y] === charWed) {
+        if (listCharacters[y].includes('18')) {
           count++;
         }
       }
