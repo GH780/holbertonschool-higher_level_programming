9b0977044b44bba6b35ab01c6200b50e7a31d091
Manuel Mosquera <mosqueramanuel5@gmail.com>
2020-05-13 02:14:39 +0000

    [Fix] Only the key is created when the task is completed. modified:   6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -2,13 +2,15 @@
 const request = require('request');
 const tasks = {};
 request(process.argv[2], (error, response, body) => {
-  if (error) { console.log(error); } else {
+  if (error) {
+    console.log(error);
+  } else {
     const list = JSON.parse(body);
     for (let x = 0; x < list.length; x++) {
-      if (!(list[x].userId in tasks)) {
-        tasks[list[x].userId] = 0;
-      }
       if (list[x].completed === true) {
+        if (!(list[x].userId in tasks)) {
+          tasks[list[x].userId] = 0;
+        }
         tasks[list[x].userId] = tasks[list[x].userId] + 1;
       }
     }
