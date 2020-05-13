57541526357787eb578a44709677d31b9ee78de2
Manuel Mosquera <mosqueramanuel5@gmail.com>
2020-05-13 03:39:43 +0000

    [Fix] To "request" isn't were affected it was necessary use "recurtion". modified:   101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,16 +1,24 @@
 #!/usr/bin/node
 const request = require('request');
-const request2 = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/';
+const listUrl = [];
 request(url + process.argv[2], (error, response, body) => {
   if (error) { console.log(error); } else {
     const listCharacters = JSON.parse(body).characters;
     for (let y = 0; y < listCharacters.length; y++) {
-      request2(listCharacters[y], (error, response, body) => {
-        if (error) { console.log(error); } else {
-          console.log(JSON.parse(body).name);
-        }
-      });
+      listUrl.push(listCharacters[y]);
     }
+    printNames(0, listUrl[0], listCharacters, listCharacters.length);
   }
 });
+
+function printNames (x, url, listUrl, size) {
+  if (x === (size)) { return; }
+  request(listUrl[x], (error, response, body) => {
+    if (error) { console.log(error); } else {
+      console.log(JSON.parse(body).name);
+      x++;
+      printNames(x, listUrl[x], listUrl, size);
+    }
+  });
+}
