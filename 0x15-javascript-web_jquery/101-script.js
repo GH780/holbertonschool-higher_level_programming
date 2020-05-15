2730fe16294abd5b75685a43a42ff76d2ed1e449
Manuel Mosquera <mosqueramanuel5@gmail.com>
2020-05-15 03:02:54 +0000

    [Feature] children() and last() are used to remove elements of a list. new file:   101-script.js

---
+++ 0x15-javascript-web_jquery/101-script.js
@@ -0,0 +1,13 @@
+window.addEventListener('load', function () {
+  $('#add_item').click(function () {
+    $('ul.my_list').append('<li>Item</li>');
+  });
+
+  $('#remove_item').click(function () {
+    $('ul.my_list li').last().remove();
+  });
+
+  $('#clear_list').click(function () {
+    $('ul.my_list').children().remove();
+  });
+});
